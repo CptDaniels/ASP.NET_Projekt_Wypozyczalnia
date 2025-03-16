63706d478e57737c7293d675ba27e924f579143c
Daniel Dwornik <44972248+CptDaniels@users.noreply.github.com>
2025-03-16 21:29:52 +0100

    Partial changes

--- Models/Klient.cs
+++ Models/Klient.cs
@@ -7,17 +7,24 @@ namespace ASP.NET_Projekt_Wypozyczalnia.Models
         [Key]
         public int IDKlienta { get; set; }
         [Required]
+        [Display(Name ="Imie")]
         public string Imie { get; set; }
         [Required]
+        [Display(Name = "Nazwisko")]
         public string Nazwisko { get; set; }
+        [Display(Name = "E-mail")]
         public string Email { get; set; }
         [Required]
+        [Display(Name = "Numer telefonu")]
         public string Telefon { get; set; }
         [Required]
+        [Display(Name = "Numer dokumentu")]
         public string NumerDokumentu { get; set; }
         [Required]
+        [Display(Name = "Typ dokumentu")]
         public string TypDokumentu { get; set; }
         [Required]
+        [Display(Name = "Adres Zamieszkania")]
         public string AdresZamieszkania { get; set; }
         //Nawigacja
         public ICollection<PozycjaWypozyczenia> PozycjeWypozyczenia { get; set; }
